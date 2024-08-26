762671c2311f67ca3f68cbb95c5169c7afc87ea7
Sead Šabanović <57287358+SeadSabanovic@users.noreply.github.com>
2024-08-27 01:04:01 +0200

    Add Title Font & BG

---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,10 @@
+import "./_background.scss";
+
+export default function Background() {
+  return (
+    <div className="background">
+      <div class="background__shape"></div>
+      <div class="background__shape"></div>
+    </div>
+  );
+}
---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,11 @@
+import "./_landing.scss";
+
+export default function Landing() {
+  return (
+    <div className="landing">
+      <h1 className="landing__title">
+        <span>Creat</span>iv<span>e</span>
+      </h1>
+    </div>
+  );
+}
